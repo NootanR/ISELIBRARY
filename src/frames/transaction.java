0533e67cb8cf9e257fe399771174afc6c88bddf0
rnootan <rnootan@rnootan-Satellite-C640>
2016-08-23 10:13:00 +0530

    frames of the tables

---
+++ src/frames/transaction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frames;
+import java.util.*;
+
+
+/**
+ *
+ * @author rnootan
+ */
+public class transaction {
+     private int id;
+    private String usn;
+     String Date ;
+    long ld=015/4/8;
+    
+    //System.out.print(ld);
+}
